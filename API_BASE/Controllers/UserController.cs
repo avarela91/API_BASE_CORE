015d6a60d139614e8402e2990eb2ba8905e7ad95
Allan Ricardo Varela <87323414+avarela91@users.noreply.github.com>
2024-08-12 19:03:20 -0600

    Add CRUD methods of users

--- API_BASE/Controllers/UserController.cs
+++ API_BASE/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Application.Services;
 using Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API_BASE.Controllers
 {
@@ -32,6 +33,59 @@ namespace API_BASE.Controllers
                 return NotFound();
             }
             return Ok(user);
+        }
+
+        [HttpPost("AddUser")]
+        public async Task<IActionResult> AddUser(User model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _userService.AddUserAsync(model);
+
+                return Ok(new {Status="success", Message = "User added successfully, UserId = "+ model.Id_User });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Status = "error", Message = "An error occurred while adding the user, Error = " + ex.Message });
+            }
+        }
+
+        [HttpPut("UpdateUser")]
+        public async Task<IActionResult> UpdateUser([FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var userById = await _userService.GetUserByIdAsync(user.Id_User);
+                if (user == null)
+                {
+                    return NotFound(new { Status = "error", Message = "User not found" });
+                }
+
+                userById.Name = user.Name;
+                userById.UserName = user.UserName;
+                userById.Password = user.Password;
+                userById.Modify_User = user.Modify_User;
+                userById.Modify_Date = user.Modify_Date;
+                userById.Active = user.Active;
+
+                await _userService.UpdateUserAsync(userById);
+
+                return Ok(new { Status = "success", Message = "User updated successfully, UserId = "+ user.Id_User });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Status = "error", Message = "An error occurred while updating the user", Error = ex.Message });
+            }
         }
 
         /*[HttpGet("GetUserPermissions")]
