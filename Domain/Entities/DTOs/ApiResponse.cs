7cffeb797575fae0db337707ec9338054f9e4d61
Allan Ricardo Varela <87323414+avarela91@users.noreply.github.com>
2024-10-10 18:25:19 -0600

    REFACTOR: Mejorar las respuestas de la api mapeadas en una clase generica

---
+++ Domain/Entities/DTOs/ApiResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.DTOs
+{
+    public class ApiResponse<T>
+    {
+        public string Status { get; set; } // "success" o "error"
+        public string Message { get; set; } // Mensaje de éxito o error
+        public T Data { get; set; } // Un objeto genérico que puede ser cualquier cosa o null
+
+        // Constructor para inicializar la respuesta
+        public ApiResponse(string status, string message, T data = default)
+        {
+            Status = status;
+            Message = message;
+            Data = data;
+        }
+    }
+}
